cfd606b3e5f185f5eed2999152bc0f573395ec6c
Anthony Lam <al3101@nyu.edu>
2018-05-06 15:57:18 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def batch_badge_creator(names) 
+  badge_messages = []
+  for name in names 
+    badge_messages.put("Hello, my name is #{name}.")
+  end 
+  return badge_messages
+end 
